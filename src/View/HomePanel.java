6664cc8cc42fb2ffd3784327435aff86a96cff22
nsubaru11 <20051126subaru@gmail.com>
2025-11-18 16:31:40 +0900

    feat: enhance HomePanel button functionality and image handling

--- src/View/HomePanel.java
+++ src/View/HomePanel.java
@@ -1,18 +1,27 @@
 package View;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Objects;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 class HomePanel extends JPanel {
-	private static final Image startImage, finishImage;
+	private static final BufferedImage startImage, finishImage;
 	private final JButton startButton;
 	private final JButton finishButton;
 
 	static {
-		startImage = new ImageIcon(Objects.requireNonNull(HomePanel.class.getResource("/Assets/start.png"))).getImage();
-		finishImage = new ImageIcon(Objects.requireNonNull(HomePanel.class.getResource("/Assets/finish.png"))).getImage();
+		try {
+			startImage = ImageIO.read(new File("/Assets/start.png"));
+			finishImage = ImageIO.read(new File("/Assets/finish.png"));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public HomePanel(OthelloGUI gui) {
@@ -36,40 +45,73 @@ class HomePanel extends JPanel {
 		gbc.insets = new Insets(10, 10, 200, 10);
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-		gbc.gridwidth = 5;
+		gbc.gridwidth = 6;
 		gbc.gridheight = 1;
 		add(titleLabel, gbc);
 
 		// 各ボタンのサイズ
-		int buttonWidth = width / 5;
-		int buttonHeight = height / 5;
+		int buttonSize = Math.min(width / 5, height / 5);
+		Dimension buttonSizeDim = new Dimension(buttonSize, buttonSize);
 
 		// finishボタンの配置
 		finishButton = new JButton();
-		Image startScaled = startImage.getScaledInstance(buttonWidth, buttonHeight, Image.SCALE_SMOOTH);
-		finishButton.setIcon(new ImageIcon(startScaled));
+		Image finishScaled = finishImage.getScaledInstance(buttonSize, buttonSize, Image.SCALE_SMOOTH);
+		finishButton.setIcon(new ImageIcon(finishScaled));
+		finishButton.setPreferredSize(buttonSizeDim);
+		finishButton.setBorderPainted(false);
 		finishButton.setContentAreaFilled(false);
-		finishButton.setPreferredSize(new Dimension(width, height));
-		gbc.insets = new Insets(10, 10, 10, 200);
-		gbc.gridx = 0;
+		finishButton.setFocusPainted(false);
+		finishButton.setRolloverEnabled(false);
+		finishButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Image finishImagePressed = finishImage.getScaledInstance((int) (buttonSize * 0.8), (int) (buttonSize * 0.8), Image.SCALE_SMOOTH);
+
+				finishButton.setIcon(new ImageIcon(finishImagePressed));
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				Image finishImageReleased = finishImage.getScaledInstance(buttonSize, buttonSize, Image.SCALE_SMOOTH);
+				finishButton.setIcon(new ImageIcon(finishImageReleased));
+			}
+		});
+		gbc.insets = new Insets(10, 200, 10, 10);
+		gbc.gridx = 1;
 		gbc.gridy = 1;
-		gbc.gridwidth = 2;
+		gbc.gridwidth = 1;
 		gbc.gridheight = 2;
 		add(finishButton, gbc);
 
 		// startボタンの配置
-		startButton = new JButton("Start");
-		startButton.setIcon(new ImageIcon(startImage));
-		startButton.setSize(new Dimension(width / 5, height / 5));
-		gbc.insets = new Insets(10, 10, 10, 0);
+		startButton = new JButton();
+		Image startScaled = startImage.getScaledInstance(buttonSize, buttonSize, Image.SCALE_SMOOTH);
+		startButton.setIcon(new ImageIcon(startScaled));
+		startButton.setPreferredSize(buttonSizeDim);
+		startButton.setBorderPainted(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setRolloverEnabled(false);
+		startButton.setFocusPainted(false);
+		startButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Image startImagePressed = startImage.getScaledInstance((int) (buttonSize * 0.8), (int) (buttonSize * 0.8), Image.SCALE_SMOOTH);
+				startButton.setIcon(new ImageIcon(startImagePressed));
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				Image startImageReleased = startImage.getScaledInstance(buttonSize, buttonSize, Image.SCALE_SMOOTH);
+				startButton.setIcon(new ImageIcon(startImageReleased));
+			}
+		});
+		gbc.insets = new Insets(10, 10, 10, 200);
 		gbc.gridx = 2;
 		gbc.gridy = 1;
-		gbc.gridwidth = 2;
+		gbc.gridwidth = 1;
 		gbc.gridheight = 2;
 		add(startButton, gbc);
-
 
 	}
 
 }
-
