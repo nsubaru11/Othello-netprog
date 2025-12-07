75588df3c5cf9fd64964ae30358d5a27193be8bd
nsubaru11 <20051126subaru@gmail.com>
2025-12-08 06:47:33 +0900

    refactor: クライアント・サーバー起動スクリプトのディレクトリ構造情報を修正
    詳細:
    - ベースディレクトリを取得する処理を `%~dp0` から `..` へ変更し、`REPO_DIR` として明確化。
    - 一時ファイル (`sources.txt`) の保存場所をスクリプトディレクトリからリポジトリディレクトリに変更。
    - 可読性と保守性向上を目的とした変更。

--- scripts/start_server.bat
+++ scripts/start_server.bat
@@ -4,14 +4,15 @@ echo Starting Othello Server...
 
 set JAVA_HOME=C:\Program Files\Amazon Corretto\jdk1.8.0_472
 set PATH=%JAVA_HOME%\bin;%PATH%
-set SRC_DIR=%~dp0src
-set OUT_DIR=%~dp0out\production\Othello-netprog
+for %%i in ("%~dp0..") do set REPO_DIR=%%~fi
+set SRC_DIR=%REPO_DIR%\src
+set OUT_DIR=%REPO_DIR%\out\production\Othello-netprog
 
 echo Compiling...
 if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
-dir /s /b "%SRC_DIR%\model\*.java" "%SRC_DIR%\common\*.java" "%SRC_DIR%\server\*.java" > "%~dp0sources.txt"
-"%JAVA_HOME%\bin\javac" -encoding UTF-8 -d "%OUT_DIR%" @"%~dp0sources.txt"
-del "%~dp0sources.txt"
+dir /s /b "%SRC_DIR%\model\*.java" "%SRC_DIR%\common\*.java" "%SRC_DIR%\server\*.java" > "%REPO_DIR%\sources.txt"
+"%JAVA_HOME%\bin\javac" -encoding UTF-8 -d "%OUT_DIR%" @"%REPO_DIR%\sources.txt"
+del "%REPO_DIR%\sources.txt"
 
 if %ERRORLEVEL% neq 0 (
     echo Compilation failed!
